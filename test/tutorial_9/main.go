26cc7e2bdcac944770fe08a32a1a508b2fa127ba
baohuy1303 <huynhbaohuy130333@gmail.com>
2026-04-08 21:30:49 -0500

    feat: implement basic channel communication using goroutines and range iteration

---
+++ test/tutorial_9/main.go
@@ -0,0 +1,17 @@
+package main
+
+func main(){
+	var channel = make(chan int) // use make keyword, chan (channel) type
+	// HAVE TO USE with goroutines because it locks and wait for var change
+	go processChannel(channel)
+	for i := range channel{ //iterate over channel 
+		println(i) // open and waiting for a value change
+	}
+	
+}
+
+func processChannel(c chan int){
+	for i := 0; i < 5; i++{
+		c <- i // assign with <- , and get value with <-
+	}
+}
